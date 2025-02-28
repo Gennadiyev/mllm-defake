46f4042b324ef8bea2ea1d7c7f07bc1fbfe59103
Kunologist <2014709936@qq.com>
2025-03-01 02:59:56 +0800

    refactor: make defake_dataset its own class to support the bbox-reason dataset

--- mllm_defake/defake_dataset/__init__.py
+++ mllm_defake/defake_dataset/__init__.py
@@ -1,1 +1,4 @@
-from .defake_dataset import RealFakeDataset, WildFakeResampled, ImageFolders
+from .images_only import ImageFolders, RealFakeDataset, WildFakeResampled
+from .images_with_bbox_and_reason import BboxReasonDataset
+
+SUPPORTED_DATASETS = ["WildFakeResampled", "ImageFolders", "WildFakeResampled20K", ""]
---
+++ mllm_defake/defake_dataset/images_with_bbox_and_reason.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class BboxReasonDataset(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def list_images(self) -> tuple[list[Path], list[Path]]:
+        """
+        This method returns a list of image file paths in the dataset.
+
+        @return: Two lists of image file paths. The first list contains the paths of real images, and the second list contains the paths of fake images.
+        """
+        return [], []
+
+    @abstractmethod
+    def get_bbox_reason(
+        self, image_path: Path
+    ) -> list[list[tuple[int, int, int, int]], str]:
+        """
+        This method returns the bounding boxes and reasons for the fake images.
+
+        @param image_path: The path of the image file.
+        @return: A tuple containing a list of bounding boxes and a string of reasons.
+        """
+        return []
