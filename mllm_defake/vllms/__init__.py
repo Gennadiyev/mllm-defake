f3ccdb86cbb330f3f0c7ec2583660636e40a2cf6
Kunologist <2014709936@qq.com>
2025-03-01 03:03:19 +0800

    docs: add full list of supported models & clarify implementations

--- mllm_defake/vllms/__init__.py
+++ mllm_defake/vllms/__init__.py
@@ -1,1 +1,13 @@
 from .vllms import *
+
+SUPPORTED_MODELS = [
+    "gpt4o",
+    "gpt4omini",
+    "gpt45",
+    "llama32vi",
+    "llavacot",
+    "qvq",
+    "internvl25",
+    "onevision",
+    "qwen2vl",
+]
