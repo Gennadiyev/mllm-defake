a09d023ebaf3e15a0a7c92063b949ea6394d6252
Kunologist <2014709936@qq.com>
2025-03-29 16:18:47 +0800

    fix: complete the rglob string

--- tests/comfor_test.py
+++ tests/comfor_test.py
@@ -12,7 +12,7 @@ def test_on_demo_images():
     from mllm_defake.classifiers.basic_classifier import ComForClassifier
 
     device = "cuda:0"
-    real_samples = Path("demo/real").rglob(")
+    real_samples = Path("demo/real").rglob("*")
     fake_samples = Path("demo/fake").rglob("*")
     community_forensics_checkpoint_path = Path("local/comfor/model_v11_ViT_224_base_ckpt.pt")
     timm_vit_checkpoint_path = Path("local/vit224")
